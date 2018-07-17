b52f30be1fb47612a160687e84eb622172ec498f
Tomwatt <tom.watt@protonmail.com>
2018-07-17 23:50:28 +0800

    Adding constants

--- drone-web/constants.js
+++ drone-web/constants.js
@@ -1,2 +1,6 @@
 exports.redisHost = 'redis'
 exports.redisPort = '6379'
+exports.droneLocationUpdateMessage = 'drone-location-update'
+exports.dronePausedNotificationMessage = 'drone-paused-notification'
+exports.droneDeletedNotificationMessage = 'drone-deleted-notification'
+exports.droneRestartedNotificationMessage = 'drone-restarted-notification'
