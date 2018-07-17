cbc62dc01e162ba6bc699fd3132710bada85da36
Tomwatt <tom.watt@protonmail.com>
2018-07-17 22:50:07 +0800

    Updating constants

--- drone-simulator/constants.js
+++ drone-simulator/constants.js
@@ -1,2 +1,8 @@
 exports.droneSocketUrl = 'http://drone-socket:3000'
 exports.simulatorPort = 3030
+exports.redisHost = 'redis'
+exports.redisPort = '6379'
+exports.droneLocationUpdateMessage = 'drone-location-update'
+exports.dronePausedNotificationMessage = 'drone-paused-notification'
+exports.droneDeletedNotificationMessage = 'drone-deleted-notification'
+exports.droneRestartedNotificationMessage = 'drone-restarted-notification'
