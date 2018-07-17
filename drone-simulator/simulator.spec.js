eaeed80d0d0521af815064ece517470a70c6f209
Tomwatt <tom.watt@protonmail.com>
2018-07-17 22:06:25 +0800

    Adding simulator test spec

---
+++ drone-simulator/simulator.spec.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect
+var sinon = require('sinon')
+var rewire = require('rewire')
+
+describe('drone-simulator simulator', function () {
+  it('createDrone tries to send new drone via socket', function () {
+
+  })
+  it('startSimulator tries to send update to existing drone', function () {
+    
+    })
+    it('pauseDrone tries to send update with paused set to true', function () {
+
+        })
+        it('restartDrone tries to send update with paused set to false', function () {
+            
+              })
+              it('deleteDrone tries to delete drone from redis', function () {
+                
+                  })
+})
