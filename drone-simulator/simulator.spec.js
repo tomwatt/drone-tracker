a79295a5b3bef16f2fc19056c8f72e7520cec2f6
Tomwatt <tom.watt@protonmail.com>
2018-07-17 22:15:46 +0800

    Formatting simulator test spec

--- drone-simulator/simulator.spec.js
+++ drone-simulator/simulator.spec.js
@@ -3,19 +3,9 @@ var sinon = require('sinon')
 var rewire = require('rewire')
 
 describe('drone-simulator simulator', function () {
-  it('createDrone tries to send new drone via socket', function () {
-
-  })
-  it('startSimulator tries to send update to existing drone', function () {
-    
-    })
-    it('pauseDrone tries to send update with paused set to true', function () {
-
-        })
-        it('restartDrone tries to send update with paused set to false', function () {
-            
-              })
-              it('deleteDrone tries to delete drone from redis', function () {
-                
-                  })
+  it('createDrone tries to send new drone via socket', function () {})
+  it('startSimulator tries to send update to existing drone', function () {})
+  it('pauseDrone tries to send update with paused set to true', function () {})
+  it('restartDrone tries to send update with paused set to false', function () {})
+  it('deleteDrone tries to delete drone from redis', function () {})
 })
