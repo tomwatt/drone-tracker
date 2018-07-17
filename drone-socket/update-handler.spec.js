1ad744f3cd2ff1b27aa80d991d03ccbfbef57b41
Tomwatt <tom.watt@protonmail.com>
2018-07-17 16:26:21 +0800

    More tests

--- drone-socket/update-handler.spec.js
+++ drone-socket/update-handler.spec.js
@@ -3,5 +3,21 @@ var expect = require('chai').expect;
 describe('drone-socket update handler', function () {
     it('tries to get existing value from redis', function () {
         
-    })
-})
+    });
+
+    it('tries to insert new value into redis', function () {
+        
+    });
+
+    it('sets speed to null if no previous value', function () {
+        
+    });
+
+    it('calculates correct speed if previous value available', function () {
+        
+    });
+
+    it('does not try to insert value if invalid data sent', function () {
+        
+    });
+});
