b3e6c8fb28b04fe2f25c8e4de25d34ab4139232a
Tomwatt <tom.watt@protonmail.com>
2018-07-17 23:43:45 +0800

    Added test spec for web app update-handler

---
+++ drone-web/update-handler.spec.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect
+var sinon = require('sinon')
+var rewire = require('rewire')
+
+describe('drone-web update handler', function () {
+  it('broadcastAllValues attempts to emit an array of drones', function () {})
+  it('subscribeToUpdates tries to emit location update on correct message', function () {})
+  it('subscribeToUpdates tries to emit paused notification on correct message', function () {})
+  it('subscribeToUpdates tries to emit restarted notification on correct message', function () {})
+  it('subscribeToUpdates tries to emit deleted notification on correct message', function () {})
+})
