39021ea9b62bc6b040ad876767b9f66c03df66cd
Tomwatt <tom.watt@protonmail.com>
2018-07-18 07:33:19 +0800

    Calling update handler from index.js in web app

--- drone-web/index.js
+++ drone-web/index.js
@@ -3,6 +3,7 @@ var app = express()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
 var path = require('path')
+var updateHandler = require('./update-handler')
 
 server.listen(80)
 
@@ -12,4 +13,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-io.on('connection', function (socket) {})
+io.on('connection', function (socket) {
+  updateHandler.broadcastAllValues(socket)
+  updateHandler.subsribeToUpdates(socket)
+})
