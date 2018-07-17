611a3f04ba28029623508004776e5953b62f3df7
Tomwatt <tom.watt@protonmail.com>
2018-07-17 17:22:44 +0800

    Adding logic to handle incoming location updates

--- drone-socket/index.js
+++ drone-socket/index.js
@@ -1,8 +1,11 @@
 var server = require('http').createServer()
 var io = require('socket.io')(server)
+var updateHandler = require('./update-handler')
 
 server.listen(3000)
 
 io.on('connection', function (socket) {
-  socket.on('drone-location-update', function (data) {})
+  socket.on('drone-location-update', function (data) {
+    updateHandler.newMessage(data)
+  })
 })
---
+++ drone-socket/update-handler.js
@@ -0,0 +1,121 @@
+var constants = require('./constants')
+var redisClient = require('redis').createClient(
+  constants.redisPort,
+  constants.redisHost
+)
+var validate = require('jsonschema').validate
+
+redisClient.on('error', function (err) {
+  console.log('Redis Client Error :' + err)
+})
+
+var messageSchema = {
+  id: '/droneMessage',
+  type: 'object',
+  properties: {
+    ID: { type: 'string' },
+    lat: { type: 'number' },
+    lon: { type: 'number' },
+    timestamp: { type: 'number' },
+    sim: { type: 'boolean' },
+    paused: { type: 'boolean' }
+  },
+  required: ['ID', 'lat', 'lon', 'timestamp']
+}
+
+// A function that takes in a JSON object representing a drone location in the form:
+// {
+//     ID: "Unique ID Here",
+//     lat: 123,
+//     lon: 456,
+//     timestamp: 123123123, // Unix timestamp
+//     sim: true, // defines whether the object was created by the simulator or a real drone
+//     paused: false //only used for simulated drones
+// }
+// Saves the object in redis with the key being set as the ID.
+// If their is a previous value for the same key, it is replaced, and the speed
+// is calculated based on the difference between the two.
+function newMessage (data) {
+  var vaildationResult = validate(data, messageSchema)
+  if (vaildationResult.valid) {
+    getExistingRedisValue(data, previousValue => {
+      var speed = calculateSpeedKmPerHour(previousValue, data)
+      data.speed = speed
+      setNewRedisValue(data)
+    })
+  }
+}
+
+// Adds data object to redis, with key set to data.ID, and publishes the update via redis
+function setNewRedisValue (data) {
+  var newValue = JSON.stringify(data)
+  redisClient.set(data.ID, newValue, (err, reply) => {
+    if (err) console.log('Error setting redis value: ' + err)
+    else redisClient.publish(constants.locationUpdateChannel, data.ID)
+  })
+}
+
+// Retrievs any existing value of data.ID in redis, parses the result and passes it to callback.
+function getExistingRedisValue (data, callback) {
+  redisClient.get(data.ID, (err, reply) => {
+    if (err) {
+      console.log('Error getting redis value: ' + err)
+      return null
+    } else {
+      var result = JSON.parse(reply)
+      callback(result)
+    }
+  })
+}
+
+// Calculates the the speed of a drone based on two location updates
+function calculateSpeedKmPerHour (previousValue, currentValue) {
+  if (previousValue) {
+    var distance = getDistanceFromLatLonInKm(
+      previousValue.lat,
+      previousValue.lon,
+      currentValue.lat,
+      currentValue.lon
+    )
+    var time = getTimeDifferenceInHoursFromMs(
+      previousValue.timestamp,
+      currentValue.timestamp
+    )
+    return distance / time
+  } else {
+    return null
+  }
+}
+
+// Get time difference in hours between two unix timestamps
+function getTimeDifferenceInHoursFromMs (previousValueMs, currentValueMs) {
+  var difference = currentValueMs - previousValueMs
+  // dont return negative values
+  if (difference <= 0) return null
+
+  return difference / constants.msPerHour
+}
+
+// Calculate distance in km along the earths surface, between two sets of coordinates.
+// Assumes the earth is spherical.
+function getDistanceFromLatLonInKm (lat1, lon1, lat2, lon2) {
+  var R = constants.radiousOfEarthInKM
+  var dLat = deg2rad(lat2 - lat1)
+  var dLon = deg2rad(lon2 - lon1)
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2)
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  var d = R * c // Distance in km
+  return d
+}
+
+// Convert degrees to radians
+function deg2rad (deg) {
+  return deg * (Math.PI / 180)
+}
+
+exports.newMessage = newMessage
