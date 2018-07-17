58a14ff3c43a7258a1661fa7814761a9407aaa06
Tomwatt <tom.watt@protonmail.com>
2018-07-17 21:44:40 +0800

    Adding test setup

---
+++ drone-simulator/test-setup.spec.js
@@ -0,0 +1,15 @@
+var sinon = require('sinon')
+var chai = require('chai')
+var sinonChai = require('sinon-chai')
+
+before(function () {
+  chai.use(sinonChai)
+})
+
+beforeEach(function () {
+  this.sandbox = sinon.createSandbox()
+})
+
+afterEach(function () {
+  this.sandbox.restore()
+})
