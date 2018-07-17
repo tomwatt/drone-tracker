11d4fe6064603eba74e7d404b1901931bc7edd36
Tomwatt <tom.watt@protonmail.com>
2018-07-17 21:47:41 +0800

    Created client socket in simulator, with empty events

--- drone-simulator/index.js
+++ drone-simulator/index.js
@@ -1,6 +1,20 @@
 var server = require('http').createServer()
 var io = require('socket.io')(server)
+var ioClient = require('socket.io-client')
+var simulator = require('./simulator')
+
+var socketUrl = 'http://drone-socket:3000'
+var simulatorSocket = ioClient(socketUrl)
+simulator.startSimulator(simulatorSocket)
 
 server.listen(3030)
 
-io.on('connection', function (socket) {})
+io.on('connection', function (socket) {
+  socket.on('create-drone-simulator', data => {})
+
+  socket.on('pause-drone', data => {})
+
+  socket.on('restart-drone', data => {})
+
+  socket.on('delete-drone', data => {})
+})
