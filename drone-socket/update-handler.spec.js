b1ce04db21560ab86f794c1c7f860822c08711b1
Tomwatt <tom.watt@protonmail.com>
2018-07-21 12:45:30 +0800

    Adding final unit test

--- drone-socket/update-handler.spec.js
+++ drone-socket/update-handler.spec.js
@@ -64,6 +64,43 @@ describe('drone-socket update handler', function () {
     revertClient()
   })
 
+  it('sets speed if previous value', function () {
+    var existingValue = {
+      ID: '123-456',
+      lat: 45,
+      lon: 45,
+      timestamp: 123123123
+    }
+
+    var newValue = {
+      ID: '123-456',
+      lat: 45.01,
+      lon: 45.01,
+      timestamp: 123123124
+    }
+
+    var existingValueInRedis = JSON.stringify(existingValue)
+
+    var updateHandler = rewire('./update-handler.js')
+
+    var redisClientMock = {
+      get: sinon.spy(function (key, cb) {
+        cb(null, existingValueInRedis)
+      }),
+      set: sinon.spy(function (key, val, cb) {})
+    }
+
+    var revertClient = updateHandler.__set__('redisClient', redisClientMock)
+
+    updateHandler.newMessage(newValue)
+
+    expect(redisClientMock.set).to.be.calledWith(existingValue.ID)
+
+    expect(JSON.parse(redisClientMock.set.args[0][1]).speed).is.not.null
+
+    revertClient()
+  })
+
   it('sets speed to null if no previous value', function () {
     var message = {
       ID: '123-456',
